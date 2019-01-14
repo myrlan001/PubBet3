7198e78cac6bc761e6b0aeb4e98004eb748067eb
JanH <jan1homola@seznam.cz>
2019-01-14 21:14:40 +0100

    my Images

--- app/src/main/java/com/developer/vaclav/pubbet/MainActivity.kt
+++ app/src/main/java/com/developer/vaclav/pubbet/MainActivity.kt
@@ -7,22 +7,19 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var op1 = '0'
+    var op2 = '0'
+    var op3 = '0'
+
+    val possibilities = listOf("000", "001", "010", "011", "100", "101", "110", "111")
+    val counters =      listOf("100", "100", "001", "001", "010", "110", "011", "011")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var op1 = '0'
-        var op2 = '0'
-        var op3 = '0'
-
-        val possibilities = listOf("000", "001", "010", "011", "100", "101", "110", "111")
-        val counters =      listOf("100", "100", "001", "001", "010", "110", "011", "011")
     }
 
     fun op1Tap(view: View) {
         op1Img.setImageResource(R.drawable.coin01)
-
-        //val bitmapDrawable = BitmapDrawable("android:resources://com.developer.vaclav.pubbet/" + R.drawable.coin01)
-        //view.setBackgroundDrawable(bitmapDrawable)
     }
 }
