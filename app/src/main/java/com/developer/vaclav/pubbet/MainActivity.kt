5e6fb80d3c22e57934177fd74bb00cc29afaf783
myrlan001 <vavasys@seznam.cz>
2019-01-14 20:32:34 +0100

    Only minor updates.

--- app/src/main/java/com/developer/vaclav/pubbet/MainActivity.kt
+++ app/src/main/java/com/developer/vaclav/pubbet/MainActivity.kt
@@ -10,9 +10,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val possibilities = listOf("000", "001", "010", "011", "100", "101", "110", "111")
-        val counters =      listOf("100", "100", "001", "001", "110", "110", "011", "011")
+        val counters =      listOf("100", "100", "001", "001", "010", "110", "011", "011")
     }
 
-    //todo note from Jenda
-
+    //todo note from  ̶J̶e̶n̶d̶a̶ Václav
 }
